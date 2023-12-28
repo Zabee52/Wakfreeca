6c96291ebed83f036ca22fba03ef70b634f649af
Yongbin Kim <zabeee52@gmail.com>
2023-12-29 02:59:55 +0900

    fix(chat): 채팅 길어지면 줄 정상적으로 바뀌지 않는 문제 (#22)
    * chore: storage-util.ts -> storage-utils.ts

--- src/chat-one-line.ts
+++ src/chat-one-line.ts
@@ -1,32 +1,34 @@
+import { elementToSpan } from './dom-utils'
+
 export default (node: HTMLElement) => {
   if (!node.hasAttribute('user_id')) {
     return
   }
-  node.style.display = 'flex'
-  node.style.alignItems = 'center'
   node.style.paddingTop = '2px'
   node.style.paddingBottom = '2px'
 
-  const nicknameSection = node.querySelector('dt')?.querySelector('a')
-  if (!nicknameSection) {
+  const nicknameArea = node.querySelector('dt')
+  if (!nicknameArea) {
     return
   }
-  const nickname = nicknameSection.childNodes[0]
+  nicknameArea.style.display = 'inline-flex'
+  nicknameArea.style.alignItems = 'center'
+  nicknameArea.style.gap = '2px'
+
+  const nickname = nicknameArea?.querySelector('a')?.childNodes[0]
   if (!nickname) {
     return
   }
-
   // 닉네임에 bold 적용
   const wrapSpan = document.createElement('span')
   wrapSpan.textContent = nickname.textContent
   wrapSpan.style.fontWeight = 'bold'
-
-  nicknameSection.insertBefore(wrapSpan, nickname)
-  nickname.textContent = ''
+  nickname.replaceWith(wrapSpan)
 
   const chatSection = node.querySelector('dd')
   if (!chatSection) {
     return
   }
-  chatSection.style.paddingLeft = '0'
+  const chatSectionSpan = elementToSpan(chatSection)
+  chatSection.replaceWith(chatSectionSpan)
 }
