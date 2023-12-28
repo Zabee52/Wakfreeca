26e1d8e1af439eb23fef0f2b55220f56bb71c1f0
Zabee52 <zabeee52@gmail.com>
2023-12-28 17:23:54 +0900

    chore(common): apply prettier

--- src/chat-one-line.js
+++ src/chat-one-line.js
@@ -1,26 +1,26 @@
 export default (node) => {
-    if (!node.hasAttribute('user_id')) {
-        return
-    }
-    node.style.display = 'flex'
-    node.style.alignItems = 'center'
-    node.style.paddingTop = '2px'
-    node.style.paddingBottom = '2px'
+  if (!node.hasAttribute('user_id')) {
+    return
+  }
+  node.style.display = 'flex'
+  node.style.alignItems = 'center'
+  node.style.paddingTop = '2px'
+  node.style.paddingBottom = '2px'
 
-    const nicknameSection = node.querySelector('dt')?.querySelector('a')
-    if (!nicknameSection) {
-        return
-    }
-    const nickname = nicknameSection.childNodes[0]
-    if (!nickname) {
-        return
-    }
+  const nicknameSection = node.querySelector('dt')?.querySelector('a')
+  if (!nicknameSection) {
+    return
+  }
+  const nickname = nicknameSection.childNodes[0]
+  if (!nickname) {
+    return
+  }
 
-    // 닉네임에 bold 적용
-    const wrapSpan = document.createElement('span')
-    wrapSpan.textContent = nickname.textContent
-    wrapSpan.style.fontWeight = 'bold'
+  // 닉네임에 bold 적용
+  const wrapSpan = document.createElement('span')
+  wrapSpan.textContent = nickname.textContent
+  wrapSpan.style.fontWeight = 'bold'
 
-    nicknameSection.insertBefore(wrapSpan, nickname)
-    nickname.textContent = ''
+  nicknameSection.insertBefore(wrapSpan, nickname)
+  nickname.textContent = ''
 }
--- src/donation-remover.js
+++ src/donation-remover.js
@@ -1,9 +1,9 @@
 export default (node) => {
-    if (!node?.classList) {
-        return
-    }
-    const removeTargetClassNames = ['balloon_area', 'adballoon_area', 'fanclub']
-    if (removeTargetClassNames.some(className => node.classList.contains(className))) {
-        node.remove();
-    }
+  if (!node?.classList) {
+    return
+  }
+  const removeTargetClassNames = ['balloon_area', 'adballoon_area', 'fanclub']
+  if (removeTargetClassNames.some((className) => node.classList.contains(className))) {
+    node.remove()
+  }
 }
