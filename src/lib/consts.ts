b9cebfeaeb9ace1c0d86aa740b4b42a065e45aa9
Zabee52 <zabeee52@gmail.com>
2023-12-30 05:18:21 +0900

    feat(chat): 세팅 UI 추가 아이템

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,3 +1,4 @@
+// Checkbox items
 export const ID_CHAT_ONE_LINE = 'chat_one_line'
 export const ID_SET_NICKNAME_COLOR = 'set_nickname_color'
 export const ID_HIDE_GENDER_ICON = 'hide_gender_icon'
@@ -7,3 +8,10 @@ export const MESSAGE_CHAT_ONE_LINE = ID_CHAT_ONE_LINE
 export const MESSAGE_SET_NICKNAME_COLOR = ID_SET_NICKNAME_COLOR
 export const MESSAGE_HIDE_GENDER_ICON = ID_HIDE_GENDER_ICON
 export const MESSAGE_HIDE_DONATION = ID_HIDE_DONATION
+
+// Chat layer items
+export const ID_CHAT_LAYER_SET_ICON = 'chat_layer_set_icon'
+export const ID_CHAT_LAYER_SET_DISPLAY_DONATION = 'chat_layer_set_display_donation'
+
+export const MESSAGE_CHAT_LAYER_SET_ICON = ID_CHAT_LAYER_SET_ICON
+export const MESSAGE_CHAT_LAYER_SET_DISPLAY_DONATION = ID_CHAT_LAYER_SET_DISPLAY_DONATION
