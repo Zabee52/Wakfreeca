77bd99e5b3bdb48eae34a9b409874ff07491ff4f
Zabee52 <zabeee52@gmail.com>
2024-01-22 22:49:44 +0900

    fix(chat): send to filter chat popup 비활성화 (미개발)

--- src/lib/send-to-filter-chat-popup.ts
+++ src/lib/send-to-filter-chat-popup.ts
@@ -1,8 +1,9 @@
 export default (node: HTMLElement) => {
-  const targetElement = document.getElementById('userFilteredChatPopupInner')
-  if (!targetElement) {
-    console.error('userFilteredChatPopupInner element not found')
-    return
-  }
-  targetElement.appendChild(node.cloneNode(true))
+  return
+  // const targetElement = document.getElementById('userFilteredChatPopupInner')
+  // if (!targetElement) {
+  //   console.error('userFilteredChatPopupInner element not found')
+  //   return
+  // }
+  // targetElement.appendChild(node.cloneNode(true))
 }
