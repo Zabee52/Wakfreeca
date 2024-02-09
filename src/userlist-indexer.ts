a6f046ecd70afd6326bccf3c3ea4abd326dd3f80
Zabee52 <zabeee52@gmail.com>
2024-02-09 20:04:07 +0900

    Type 1

--- src/userlist-indexer.ts
+++ src/userlist-indexer.ts
@@ -2,6 +2,41 @@ import { UserInformation } from './lib/interfaces'
 
 const userLists: Record<string, UserInformation> = {}
 
+const userListIndexCallback = function (mutationsList: MutationRecord[], observer: MutationObserver) {
+  for (const mutation of mutationsList) {
+    if (mutation.type !== 'childList') {
+      continue
+    }
+
+    mutation.addedNodes.forEach(async (node: Node) => {
+      if (!(node instanceof HTMLElement) || node.classList[0] === 'title') {
+        return
+      }
+
+      const mutateTargetNode = node.querySelector('a')
+      if (!mutateTargetNode) {
+        return
+      }
+
+      const nickname = mutateTargetNode.getAttribute('user_nick') || ''
+
+      if (userLists[nickname]) {
+        return
+      }
+
+      const userInformation: UserInformation = {
+        grade: mutateTargetNode.getAttribute('grade') || '',
+        nickname,
+        userId: mutateTargetNode.getAttribute('user_id') || '',
+      }
+
+      userLists[nickname] = userInformation
+    })
+  }
+}
+
+const userListIndexer = new MutationObserver(userListIndexCallback)
+
 function userListIndexerInit() {
   const scrollTarget = document.getElementById('userList')
   if (!scrollTarget) {
@@ -14,42 +49,21 @@ function userListIndexerInit() {
   }
 
   const observerConfig = { attributes: false, childList: true, subtree: false }
-
-  const callback = function (mutationsList: MutationRecord[], observer: MutationObserver) {
-    for (const mutation of mutationsList) {
-      if (mutation.type !== 'childList') {
-        continue
-      }
-
-      mutation.addedNodes.forEach(async (node: Node) => {
-        if (!(node instanceof HTMLElement) || node.classList[0] === 'title') {
-          return
-        }
-
-        const mutateTargetNode = node.querySelector('a')
-        if (!mutateTargetNode) {
-          return
-        }
-
-        const nickname = mutateTargetNode.getAttribute('user_nick') || ''
-
-        if (userLists[nickname]) {
-          return
-        }
-
-        const userInformation: UserInformation = {
-          grade: mutateTargetNode.getAttribute('grade') || '',
-          nickname,
-          userId: mutateTargetNode.getAttribute('user_id') || '',
-        }
-
-        userLists[nickname] = userInformation
-      })
-    }
-  }
-
-  const observer = new MutationObserver(callback)
-  observer.observe(ingestTarget, observerConfig)
+  userListIndexer.observe(ingestTarget, observerConfig)
 }
 
+async function triggerUserListIndex() {
+  const userList = document.getElementById('userList')
+  if (!userList) {
+    throw new Error('userList element not found')
+  }
+
+  const elemHeight = userList.clientHeight
+  const maxHeight = userList.scrollHeight
+  
+  for (let i = elemHeight; i < maxHeight; i += elemHeight) {
+    userList.scrollTop = i
+  }
+}
 // userListIndexerInit()
+// triggerUserListIndex()
