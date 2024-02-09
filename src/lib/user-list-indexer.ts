28e88cbb2e599e18fae50d509b1c44cf821788cb
Zabee52 <zabeee52@gmail.com>
2024-02-10 08:16:08 +0900

    feat(chat): 사용자 검색 기능

---
+++ src/lib/user-list-indexer.ts
@@ -0,0 +1,34 @@
+import { UserInformation } from './interfaces'
+import { getConnectedLiveView, getCurrentViewerList } from './live-view-utils'
+import { TrieFactory } from './trie-factory'
+
+const userList = TrieFactory.getInstance<UserInformation>()
+
+const ingest = (grade: string, nickname: string, userId: string) => {
+  const userInfo: UserInformation = {
+    grade,
+    nickname,
+    userId,
+  }
+
+  return userList.insert(nickname, userInfo)
+}
+
+export const indexUserList = async () => {
+  const liveView = await getConnectedLiveView()
+  if (!liveView?.playerController?.sendChUser) {
+    throw new Error('liveView not found')
+  }
+
+  const viewerList = await getCurrentViewerList(liveView)
+  if (!viewerList?.length) {
+    console.log('index failed')
+    return null
+  }
+
+  viewerList.forEach((viewer) => {
+    ingest(viewer.grade, viewer.nickname, viewer.id)
+  })
+
+  return userList
+}
